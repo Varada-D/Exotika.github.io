bc3cb912bbeb6d50f7df600080529026d66ab8be
Varada-D <varadada@rediffmail.com>
2021-08-30 23:42:08 +0530

    Exotika: A complete marketplace for Resorts Pan India

---
+++ JS/NavigationBar.js
@@ -0,0 +1,33 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+function myFunction() {
+	var x = document.getElementById("navbar");
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	}
+
+	else {
+		x.className = "topnav";
+	}
+}
+
+
+
+
+/* Shrink the header on scroll */
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+	if (document.body.scrollTop>80 || document.documentElement.scrollTop>80)
+		{
+			document.getElementById('logo').style.width = "25vh";
+			document.getElementsById('SignInRegisterBtnLS').style.marginTop = "2.5vh";
+		}
+	else
+		{
+			document.getElementById('logo').style.width = "45vh";
+			document.getElementsById('SignInRegisterBtnLS').style.marginTop = "5vh";
+		}
+}
